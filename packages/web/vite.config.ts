50b8ecaa8810700246dfba91b00bf81ae2e19271
root <root@ser9101787681.local>
2026-05-06 09:13:26 +0000

    fix: vite.config.ts absolute paths

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from '/root/.local/share/fnm/node-versions/v25.9.0/installation/lib/node_modules/vite/dist/node/index.js'
-import react from '/root/.local/share/fnm/node-versions/v25.9.0/installation/lib/node_modules/@vitejs/plugin-react/dist/index.js'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
